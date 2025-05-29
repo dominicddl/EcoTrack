bce25d5463f5401eb67f77cd65dcc4f453f0906b
dominicddl <e1398293@u.nus.edu>
2025-05-29 23:57:55 +0800

    added useMediaQuery

---
+++ hooks/useMediaQuery.ts
@@ -0,0 +1,16 @@
+import { useState, useEffect } from "react";
+
+export function useMediaQuery(query: string): boolean {
+   const [matches, setMatches] = useState<boolean>(false);
+
+   useEffect(() => {
+        const mediaQueryList = window.matchMedia(query);
+        if (mediaQueryList.matches !== matches) {
+            setMatches(mediaQueryList.matches);
+        }
+        const listener = () => setMatches(mediaQueryList.matches);
+        mediaQueryList.addListener(listener);
+        return () => mediaQueryList.removeListener(listener);
+   }, [query, matches]);
+    return matches;
+}
